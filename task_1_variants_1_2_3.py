ed1a832ceb7ffbae528358ee2af9aa61670e0787
rosty <rostyslav.furman@gmail.com>
2022-09-01 16:43:59 +0300

    Latest version

--- task_1_variants_1_2_3.py
+++ task_1_variants_1_2_3.py
@@ -1,11 +1,17 @@
 import hashlib as hs
-
+#variant 1 
 s_1 = b"Python Bootcamp"
 s_hashing_variant_1 = hs.md5(s_1)
-
 print('Variant 1:',s_hashing_variant_1.hexdigest())
 
 
+#variant2
 s_2 = b"Python Bootcamp"
 s_hashing_variant_2 = hs.sha256(s_2)
 print('Variant 2:',s_hashing_variant_2.hexdigest())
+
+
+#variant3
+s_3 = b"Python Bootcamp"
+s_hashing_variant_3 = hash(s_3)
+print('Variant 3:', s_hashing_variant_3)
