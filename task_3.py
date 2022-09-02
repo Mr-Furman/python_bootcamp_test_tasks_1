1517b30fd4fd61181f6de2752ef270e20975f5a4
Rostyslav Furman <106009301+Mr-Furman@users.noreply.github.com>
2022-09-02 15:38:32 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -17,37 +17,30 @@ class Film:
             self.films = pd.concat([self.films, pd.read_csv(path_to_file)])
 
     def add_note(self):
-      """Add note to .csv file"""
 
-      counter_of_string = 0
-      for i in self.films['film_name']:
-        if i == self.film_name:
-          counter_of_string += 1
+      if self.film_name in self.films['film_name'].values:
           print(f'We have {self.film_name} in our library')
-        
-      if counter_of_string == 0:  
+      else:
           film = {"film_name": [self.film_name], " rating": [self.rating], " note": [self.note]}
           self.films = pd.concat([self.films, pd.DataFrame.from_dict(film)], ignore_index=True)
           self.films.to_csv("Book1.csv", index=False)
 
 
     def remove_note(self):
-        """Remove note from .csv file"""
         self.films = self.films[~((self.films["film_name"]==self.film_name) & (self.films[" rating"]==self.rating) & (self.films[" note"]==self.note))]
         self.films.to_csv("Book1.csv", index=False)
 
     def print_notes_to_console(self):
-        
         print(self.films)
 
     def highest_rating(self):
       """Get films with the highest rating"""
-      print(self.films[self.films[' rating'] == 5])
+      print(self.films[self.films[' rating'] == self.films[' rating'].max()])
    
    
     def lowest_rating(self):
       """Get films with the lowest rating"""
-      print(self.films[self.films[' rating'] == 1])
+      print(self.films[self.films[' rating'] == self.films[' rating'].min()])
 
     def middle_of_all(self):
       """Get average rating among all films"""
@@ -58,7 +51,7 @@ class Film:
 
 
 film_1 = Film('Speed', 5, 'norm')
-film_1.read_csv("o:\it\Book1.csv")
+film_1.read_csv('Book1.csv')
 film_1.add_note()
 
 film_1.print_notes_to_console()
